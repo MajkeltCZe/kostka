f9b5390e921bb5bfd12220caeb5e8c9850b28645
MajkeltCZe <majkeltcz@gmail.com>
2021-10-26 11:55:58 +0200

    02 statistiky

--- js/game.js
+++ js/game.js
@@ -5,15 +5,78 @@ const button = document.getElementById('button');
 let hod = 1;
 let hody = [];
 
+let soucet = 0;
+let prumer = 0;
+
+
 button.addEventListener('click', function() {
 hod = Math.ceil(Math.random() * 6);
 hody.push(hod);
-console.log(hody);
+
 kostka.src = './img/kostka' + hod + '.png';
+statistikyVypis ();
+soucet += hod;
+prumer = soucet / hody.length;
 
 
 
 
 });
 
-function statistikyVypis ()
+/*
+function Soucet() {
+
+let sum = 0;
+
+for (let i = 0;i < hody.length;i++) {
+sum += hody[i];
+
+}
+return sum;
+
+console.log(soucet);
+
+}
+*/
+
+
+
+function maximum() {
+
+let max = 1;
+
+hody.forEach(function(value,index){
+
+if(value  > max) max = value;
+})
+
+return max;
+
+
+}
+
+function minimum() {
+
+    let min =  6 ;
+    
+    hody.forEach(function(value,index){
+    
+    if(value  < min) min = value;
+    })
+    
+    return min;
+    
+    
+    }
+
+
+function statistikyVypis () {
+statistika.innerHTML = `<p>Poslední hod: ${hod}</p>`; 
+statistika.innerHTML += `<p>Celkový počet hodů: ${hody.length}</p>` ;
+statistika.innerHTML += `<p>Součet: ${soucet}</p>` ;
+statistika.innerHTML += `<p>Průměr: ${(prumer).toFixed(2)}</p>` ;
+statistika.innerHTML += `<p>Maximum: ${maximum()}</p>` ;
+statistika.innerHTML += `<p>Minimum: ${minimum()}</p>` ;
+}
+
+
